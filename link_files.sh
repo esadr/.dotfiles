5eb6e9374275324ec2c8e34baa5756100823326d
Justin Sybrandt <nalta45@gmail.com>
2019-01-04 18:58:20 -0500

    Adds i3 configs
    Introduces gaps settings and .wallpaper
    i3 is now going to setup x

--- link_files.sh
+++ link_files.sh
@@ -4,10 +4,12 @@
 # i3 config
 mkdir -p ~/.config/i3
 ln -sf $(readlink -f i3/config) ~/.config/i3/config
+ln -sf $(readlink -f i3/wallpaper.jpg) ~/.config/i3/wallpaper.jpg
 
 # xinit
 ln -sf $(readlink -f xsettings/xinitrc) ~/.xinitrc
 ln -sf $(readlink -f xsettings/xinitrc) ~/.xsession
+ln -sf $(readlink -f xsettings/xinitrc) ~/.Xresources
 mkdir -p ~/.screenlayout
 ln -sf $(readlink -f xsettings/two_monitor_vertical_and_horizontal.sh) \
   ~/.screenlayout/two_monitor_vertical_and_horizontal.sh
