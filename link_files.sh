eac1d7a0c86f7a8890332513b9d99d125cec7181
Justin Sybrandt <nalta45@gmail.com>
2019-01-04 17:23:05 -0500

    Adds old vim, basic i3, and xinitrc
    This commit recreates the .vim repo.
    Submodules here are now linked via http for easier cloning to new
    machines.
    
    xinitrc is created to map caps lock to esc
    
    i3 basic has an empty area for custom commands.

---
+++ link_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LINK_FLGS=-sf
+
+# i3 config
+ln $LINK_FLGS $(readlink -f i3/config) ~/.config/i3/config
+
+# xinit
+ln $LINK_FLGS $(readlink -f xinitrc) ~/.xinitrc
+
+# vim
+ln $LINK_FLGS $(readlink -f vim) ~/.vim
+ln $LINK_FLGS $(readlink -f vim/vimrc) ~/.vimrc
